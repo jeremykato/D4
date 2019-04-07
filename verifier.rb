2253849fae109039804a67f3a73b8cc4c514d8dc
Jeremy Kato <jeremykato@hotmail.com>
2019-04-06 23:00:06 -0400

    bugfix

--- verifier.rb
+++ verifier.rb
@@ -11,13 +11,21 @@ elsif !File.file?(ARGV[0])
   return
 end
 
+print = 0
+
 Parallel.map([0, 1, 2, 3], in_threads: 4) do |p|
   # the verifier object depends on what file I've included at the top
   v = Verifier.new(IO.readlines(ARGV[0]), p)
   v.process
   if !v.success?
     v.put_result
-  elsif p == 2
-    v.put_result
+    print -= 5
+  else
+    print += 1
+  end
+  if p == 2
+    while print != 4 && print > 0 do
+    end
+    v.put_result if print == 4
   end
 end
